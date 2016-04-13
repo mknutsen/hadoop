532de4c961a7b34c5adf08b4f9af7996be4d4b73
Max Knutsen <mknutsen99@gmail.com>
2016-04-12 22:54:03 -0400

    changed tweet load to read avro files

--- src/main/java/twitter/accumulo/HashtagsLoad.java
+++ src/main/java/twitter/accumulo/HashtagsLoad.java
@@ -16,22 +16,19 @@ public class HashtagsLoad extends DataLoad {
     public static void main(String args[]) throws Exception {
         System.exit(ToolRunner.run(new Configuration(), new HashtagsLoad(), args));
     }
-    private static String specialCharacter = null;
+
     //File containing many rows of data, columns <tweet id> <hashtag> <count>, ordered by count descending
     @Override
     Mutation processLine(final String rawString) {
-	for(int i=0;i<rawString.length() && specialCharacter==null;i++){
-	    if(!(rawString.charAt(i) >= 0 && rawString.charAt(i) <= 9)){
-		specialCharacter = rawString.substring(i,i+1);
-            }
-        }
+
 
         String[] parts = rawString.split("[^\\w']+");
-	for(int i =0;i<parts.length;i++) System.out.println(parts[i]);
+
         Mutation mutation = new Mutation(parts[0]);
         mutation.put("hashtag", "tweet_id", parts[0]);
         mutation.put("hashtag", "hashtag", parts[1]);
         mutation.put("hashtag", "count", parts[2]);
         return mutation;
+
     }
 }
--- src/main/java/twitter/accumulo/ReverseIndexLoad.java
+++ src/main/java/twitter/accumulo/ReverseIndexLoad.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.util.ToolRunner;
  */
 public class ReverseIndexLoad extends DataLoad {
 
+
     public ReverseIndexLoad() {
         super("reverse_index");
     }
@@ -17,16 +18,11 @@ public class ReverseIndexLoad extends DataLoad {
     public static void main(String args[]) throws Exception {
         System.exit(ToolRunner.run(new Configuration(), new HashtagsLoad(), args));
     }
-    private static String specialCharacter=null;
+
     @Override
     Mutation processLine(final String rawString) {
-        for(int i=0;i<rawString.length() && specialCharacter==null;i++){
-	    if(!(rawString.charAt(i) >= 0 && rawString.charAt(i) <= 9)){
-		specialCharacter = rawString.substring(i,i+1);
-            }
-        }
 
-        String[] parts = rawString.split(specialCharacter);
+        String[] parts = rawString.split("[^\\w']+");
         Mutation mutation = new Mutation(parts[1]);
         mutation.put("word", "word", parts[0]);
         mutation.put("word", "tweet_id", parts[1]);
